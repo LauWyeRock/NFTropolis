6ee8f1250f5f308afac06c8e06e8aba939508309
LauWyeRock <97499447+LauWyeRock@users.noreply.github.com>
2023-03-19 04:06:12 +0800

    Everything

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+const NEXT_PUBLIC_POLYGON_MUMBAI_RPC = "https://polygon-mumbai.g.alchemy.com/v2/O3kCCuCNKae145765zyl0DANkC4L8zaF";
+const NEXT_PUBLIC_PRIVATE_KEY = "a30c5725940d06641e1fa7af2397834c1e9561d9b9827dde466513f56881dd7c";
+/** @type import('hardhat/config').HardhatUserConfig */
+
+module.exports = {
+  solidity: "0.8.17",
+  defaultNetwork: "matic",
+  networks: {
+    hardhat: {},
+    polygon_mumbai: {
+      url: NEXT_PUBLIC_POLYGON_MUMBAI_RPC,
+      accounts: [`0x${NEXT_PUBLIC_PRIVATE_KEY}`],
+    },
+  },
+
+    // fuji: {
+    //   url: `Your URL`,
+    //   accounts: [
+    //     `0x${"Your Account"}`,
+    //   ],
+    // },
+    // },
+};
--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify:true,
+  images: {
+    domains: ["is4302.infura-ipfs.io", "infura-ipfs.io"],
+    formats: ["image/webp"],
+  }
 }
 
 module.exports = nextConfig
